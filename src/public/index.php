e48f3a5e892bccf67412928358dc1277fbeacf8e
Emir <a.shadanfar.IT@gmail.com>
2022-07-08 15:24:46 +0430

    load eloquent in project's bootstrap file

--- src/public/index.php
+++ src/public/index.php
@@ -2,5 +2,16 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use Illuminate\Database\Capsule\Manager as Capsule;
 
-echo "Hello";
+// Eloquent
+$capsule = new Capsule;
+$capsule->addConnection([
+    "driver" => "mysql",
+    "host" => "127.0.0.1",
+    "database" => "blog",
+    "username" => "root",
+    "password" => "password"
+]);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
