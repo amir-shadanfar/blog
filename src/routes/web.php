9d73e1b0daae64e0f49ffc5579eb4551262c13e5
Emir <a.shadanfar.IT@gmail.com>
2022-07-08 18:14:27 +0430

    fix router

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,7 @@
 
 use Pecee\SimpleRouter\SimpleRouter as Router;
 
-//Router::csrfVerifier(new \Demo\Middlewares\CsrfVerifier());
+// Router::csrfVerifier(csrf_token());
 
 Router::group(['namespace' => '\App\Controllers'], function () {
     // pages
@@ -10,8 +10,6 @@ Router::group(['namespace' => '\App\Controllers'], function () {
     Router::get('/blogs', [\App\Controllers\PageController::class, 'blogs']);
     Router::get('/blog/{id}', [\App\Controllers\PageController::class, 'blog_detail']);
     Router::get('/about', [\App\Controllers\PageController::class, 'about']);
-    Router::get('/not-found', '\App\Controllers\PageController@notFound');
-    Router::get('/forbidden', '\App\Controllers\PageController@notFound');
     // methods
     Router::post('/blog', [\App\Controllers\ActionController::class, 'store_blog']);
     Router::delete('/comment/{id}', [\App\Controllers\ActionController::class, 'delete_comment']);
