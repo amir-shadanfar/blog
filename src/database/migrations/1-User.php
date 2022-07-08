9fb758c0f305bdbde9985748857e805322eb4c5f
Emir <a.shadanfar.IT@gmail.com>
2022-07-08 15:26:48 +0430

    create migrations

---
+++ src/database/migrations/1-User.php
@@ -0,0 +1,13 @@
+<?php
+
+require "../../public/index.php";
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+Capsule::schema()->create('users', function ($table) {
+    $table->increments('id');
+    $table->string('name');
+    $table->string('email')->unique();
+    $table->string('password');
+    $table->timestamps();
+});
