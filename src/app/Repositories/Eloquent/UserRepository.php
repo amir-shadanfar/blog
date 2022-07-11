cccd9635ed812316e8106e227eaedcfee32415a1
Emir <a.shadanfar.IT@gmail.com>
2022-07-11 19:30:33 +0430

    add auth repository

--- src/app/Repositories/Eloquent/UserRepository.php
+++ src/app/Repositories/Eloquent/UserRepository.php
@@ -1,13 +1,12 @@
 <?php
 
-namespace App\Repository\Eloquent;
+namespace App\Repositories\Eloquent;
 
 use App\Models\User;
-use App\Repository\UserRepositoryInterface;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
-use App\Repository\BaseRepository;
+use App\Repositories\BaseRepository;
 
 class UserRepository extends BaseRepository implements UserRepositoryInterface
 {
@@ -42,5 +41,51 @@ class UserRepository extends BaseRepository implements UserRepositoryInterface
             });
         }
         return $query;
+    }
+
+    /**
+     * @param string $email
+     * @param string $password
+     *
+     * @return \App\Models\User
+     * @throws \Exception
+     */
+    public function login(string $email, string $password): User
+    {
+        $user = $this->model->where('email', $email)->first();
+        if (!$user) {
+            throw new \Exception('Email or Password is incorrect!');
+        }
+        if ($user) {
+            if (password_verify($password, $user->password)) {
+                // login successful
+                $_SESSION["user_id"] = $user->id;
+                $_SESSION["user_name"] = $user->name;
+
+                return $user;
+            }
+        }
+    }
+
+    public function logout(): void
+    {
+
+    }
+
+    /**
+     * @param string $name
+     * @param string $email
+     * @param string $password
+     *
+     * @return \App\Models\User
+     */
+    public function register(string $name, string $email, string $password): User
+    {
+        $user = new User;
+        $user->name = $name;
+        $user->email = $email;
+        $user->password = password_hash($password, PASSWORD_DEFAULT);
+        $user->save();
+        return $user;
     }
 }
--- src/app/Repositories/Eloquent/UserRepositoryInterface.php
+++ src/app/Repositories/Eloquent/UserRepositoryInterface.php
@@ -1,10 +1,16 @@
 <?php
 
-namespace App\Repository;
+namespace App\Repositories\Eloquent;
 
+use App\Models\User;
+use App\Repositories\BaseRepositoryInterface;
 use Illuminate\Database\Eloquent\Model;
 
-interface UserRepositoryInterface extends RepositoryInterface
+interface UserRepositoryInterface extends BaseRepositoryInterface
 {
+    public function login(string $email, string $password): User;
 
+    public function register(string $name, string $email, string $password): User;
+
+    public function logout(): void;
 }
